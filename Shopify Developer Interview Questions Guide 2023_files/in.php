5e3f8f82ed999e54107ddf70a22045d485c0cf53
Branko Jovanovic <brankoweb98@gmail.com>
2025-03-30 11:32:36 -0700

    init

---
+++ Shopify Developer Interview Questions Guide 2023_files/in.php
@@ -0,0 +1,7 @@
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '1695516489');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'https://www.google.com/', 30*86400 );
+}
